789a9f181f629fcda6d65a69bf142b53b88ebf70
Ramil <ramil.trli1999@mail.ru>
2022-12-02 20:32:59 +0300

    first commit

---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,18 @@
+import { NoteData, Tag } from "../App"
+import { NoteForm } from "../components/NoteForm"
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+export const NewNote = ({onSubmit, onAddTag, availableTags}: NewNoteProps) => {
+  return (
+    <>
+    <h1 className="mb-4">NewNote</h1>
+    <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
+    </>
+  )
+}
+
